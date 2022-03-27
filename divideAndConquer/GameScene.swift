587b420c6d2417c8d9ff5475624e0eaa412e09d3
Gary Rubinstein <garyrubinstein@yahoo.com>
2022-03-26 23:59:13 -0400

    testing git

--- divideAndConquer/GameScene.swift
+++ divideAndConquer/GameScene.swift
@@ -13,5 +13,7 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         let testBlock = gameBlock()
         print(testBlock.getDivisor())
+        print(testBlock.getDivisor())
+        print(testBlock.getQuotient())
     }
 }
