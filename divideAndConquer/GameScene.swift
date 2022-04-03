e13892c1a0a2eca087205b876c07148a3a84f158
Gary Rubinstein <garyrubinstein@yahoo.com>
2022-04-03 12:48:10 -0400

    working on physics

--- divideAndConquer/GameScene.swift
+++ divideAndConquer/GameScene.swift
@@ -9,11 +9,41 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
+    /*
     override func didMove(to view: SKView) {
-        let testBlock = gameBlock()
+        let testBlock = gameBlock(color: UIColor.white, size: CGSize(width: 300.0, height: 100.0))
+        // testBlock.color = UIColor.white
+        testBlock.position.x = 0
+        testBlock.position.y = 0
         print(testBlock.getDivisor())
         print(testBlock.getDivisor())
         print(testBlock.getQuotient())
+        self.addChild(testBlock)
+    }
+ */
+    override func didMove(to view: SKView) {
+        backgroundColor = UIColor.white
+
+        scene!.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        // var i = 0
+        
+        //createBall()
+        //run(SKAction.repeat(SKAction.sequence())
+        // run(SKAction.repeat(SKAction.sequence([SKAction.run(createBall), SKAction.wait(forDuration: 0.05)]), count: 200))
+    }
+    func createBall() {
+        //let ball = SKSpriteNode(imageNamed: "ball")
+        let ball = SKSpriteNode(color: UIColor.red, size: CGSize(width: 200.0, height: 100.0))
+        var xPos = CGFloat.random(in: 0...50)
+        ball.position = CGPoint(x: xPos, y: 0.0)
+        // ball.position = CGPoint(x: CGFloat(Int(arc4random()) & Int(size.width)), y: size.height - ball.size.height)
+        ball.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 200.0, height: 100.0))
+        // ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width/2)
+        addChild(ball)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("touched")
+        createBall()
     }
 }
--- divideAndConquer/gameBlock.swift
+++ divideAndConquer/gameBlock.swift
@@ -10,12 +10,24 @@ class gameBlock: SKSpriteNode {
     private var dividend: Int = 408
     private var divisor: Int = 17
     private var quotient: Int = 24
+    private var theHeight: CGFloat = 100
+    private var theWidth: CGFloat = 300
     
     func initializeGameBlock () {
+        // self.size =  = theWidth
+        // self.height = theHeight
+        // var barra = SKSpriteNode()
+        self.name = "tester"
+        self.size = CGSize(width: 300.0, height: 100.0)
+        // self.color = SKColor.green
+        // self.position = CGPoint(x: 100, y: 100)
+        // self.addChild(barra)
+        /*
         self.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: self.size.width-50, height: self.size.height-5))
         self.physicsBody?.affectedByGravity = true
         self.physicsBody?.allowsRotation = false
         self.physicsBody?.restitution = 0
+        */
         // self.physicsBody?.categoryBitMask = ColliderType.Player
         // self.physicsBody?.collisionBitMask = ColliderType.Cloud
         // self.physicsBody?.contactTestBitMask = ColliderType.DarkCloudAndCollectibles
