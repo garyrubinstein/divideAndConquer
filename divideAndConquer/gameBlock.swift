41f711e4e1a8e532f9285b654757f8d8ddecf01f
Gary Rubinstein <garyrubinstein@yahoo.com>
2022-04-09 01:24:52 -0400

    made functions for turning the numbers into arrays so they can be put into the blocks.  Made it so the blocks get random divisor, dividend, and quotients.  Next step need to put the negative signs if appropriate and then make it so the quotient can be changed.

--- divideAndConquer/gameBlock.swift
+++ divideAndConquer/gameBlock.swift
@@ -9,7 +9,8 @@ import SpriteKit
 class gameBlock: SKSpriteNode {
     private var dividend: Int = 408
     private var divisor: Int = 17
-    private var quotient: Int = 24
+    private var quotient: Int = 21 // what they have for it, not the actual answer
+    private var theAnswer: Int = 24
     private var theHeight: CGFloat = 100
     private var theWidth: CGFloat = 300
     private var ballLabel: SKLabelNode = SKLabelNode()
@@ -18,18 +19,25 @@ class gameBlock: SKSpriteNode {
     private var theTextNodes: [SKLabelNode] = []
     
     func initializeGameBlock () {
+        self.theAnswer = Int.random(in: 10..<100)
+        self.divisor = Int.random(in: 10..<100)
+        self.dividend = self.theAnswer*self.divisor
+        self.quotient = 55
         self.isUserInteractionEnabled = false
         print("initializeGameBlock")
+        print("testing number to array 12,4")
+        print(self.numberToArray(n: 12, p: 4))
         // self.size =  = theWidth
         // self.height = theHeight
         // var barra = SKSpriteNode()
-        self.theNumbers = [9,9,9,9,9,8,0,1,8,9,1,8,9,1,8,9,1,0,0,0]
-        let thePositions = [5,6,7,8,9,10,11,12,15,16,17,22,23,24,28,29,30,34,35,36]
+        setNumbers()
+        // self.theNumbers = setNumbers() //[0,0,9,9,9,9,9,8,0,1,8,9,1,0,8,9,1,0,8,9,1,0,0,0,0]
+        let thePositions = [3,4,5,6,7,8,9,10,11,12,15,16,17,21,22,23,24,27,28,29,30,33,34,35,36]
         self.name = "tester"
         self.size = CGSize(width: sideLen, height: sideLen)
         self.color = UIColor.blue
-        for i in 0...19 {
-            let numLabel = SKLabelNode(text: String(theNumbers[i]))
+        for i in 0...24 {
+            let numLabel = SKLabelNode(text: "a") // String(theNumbers[i]))
             numLabel.fontName = "AvenirNext-Bold"
             numLabel.fontSize = 36.0
             numLabel.name = "n"+String(i)
@@ -39,6 +47,7 @@ class gameBlock: SKSpriteNode {
             // numLabel.position = CGPoint(x: thePositions[i-1]%6*Int(self.theWidth), y: Int(thePositions[i-1]/6)*Int(self.theWidth))
             self.addChild(numLabel)
         }
+        updateNumbers()
         // let ball = SKSpriteNode(color: UIColor.red, size: CGSize(width: 200.0, height: 100.0))
         ballLabel.text = "hi there"
         // let ballLabel = SKLabelNode(text: "hello")
@@ -81,6 +90,36 @@ class gameBlock: SKSpriteNode {
     }
     func setName(s: String) {
         self.name = s
+    }
+    func setNumbers() {
+        // there will be 25 numbers in this
+        let (tens,ones)=self.quotient.quotientAndRemainder(dividingBy: 10)
+        self.theNumbers=[]
+        self.theNumbers.append(contentsOf: numberToArray(n: self.quotient, p: 4))
+        self.theNumbers.append(contentsOf: numberToArray(n: self.divisor, p: 2))
+        self.theNumbers.append(contentsOf: numberToArray(n: self.dividend, p: 4))
+        self.theNumbers.append(contentsOf: numberToArray(n: tens*self.divisor, p: 3))
+        self.theNumbers.append(contentsOf: numberToArray(n: abs(self.dividend-10*tens*self.divisor), p: 4))
+        self.theNumbers.append(contentsOf: numberToArray(n: ones*self.divisor, p: 4))
+        self.theNumbers.append(contentsOf: numberToArray(n: abs(self.dividend-quotient*divisor), p: 4))
+    }
+    func numberToArray(n: Int, p: Int) -> [Int] {
+        var n1 = n
+        var retArray: [Int] = []
+        var i = 0
+        while (i<p) {
+            retArray.insert(n1%10, at: 0)
+            var (q,r)=n1.quotientAndRemainder(dividingBy: 10)
+            n1 = q
+            // retArray = [n%10]+retArray
+            i=i+1
+        }
+        return retArray
+    }
+    func updateNumbers() {
+        for i in 0...24 {
+            self.theTextNodes[i].text = String(self.theNumbers[i])
+        }
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         print(self.name)
